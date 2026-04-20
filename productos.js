5f9add7f848c178629517b6dd3000e58f919cb29
Elias Emanuel Almada <124707045+EliasIchi@users.noreply.github.com>
2026-04-19 22:55:17 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,18 +1,14 @@
 const productos=[
-{
-nombre:"Pizza Muzzarella",
-precio:12000
-},
-{
-nombre:"Pizza Jamon y Morron",
-precio:14500
-},
-{
-nombre:"Empanada JyQ",
-precio:1200
-},
-{
-nombre:"Ravioles",
-precio:8500
-}
-]
+{id:1,nombre:"Muzzarella",cat:"Pizzas",precio:15000},
+{id:11,nombre:"Jamon y Morrón",cat:"Pizzas",precio:22000},
+{id:23,nombre:"4 quesos",cat:"Pizzas",precio:24000},
+{id:236,nombre:"Empanadas x6",cat:"Empanadas",precio:18000},
+{id:219,nombre:"Empanadas x12",cat:"Empanadas",precio:30000},
+{id:231,nombre:"Empanada Jamón y Queso",cat:"Empanadas",precio:3000},
+{id:34,nombre:"Empanada de Carne",cat:"Empanadas",precio:3000},
+{id:33,nombre:"Empanada de Pollo",cat:"Empanadas",precio:3000},
+{id:46,nombre:"Cintas Caseras c/Salsa Clásica",cat:"Pastas",precio:16000},
+{id:241,nombre:"Ñoquis c/Salsa Clásica",cat:"Pastas",precio:16000},
+{id:44,nombre:"Coca Cola 1.75",cat:"Bebidas",precio:5500},
+{id:40,nombre:"Manaos Lima 2.25",cat:"Bebidas",precio:2800}
+];
